6a3874fc90bb953390be3999ca7789a0c6d4b01a
antonynans <antonynans@gmail.com>
2023-02-03 19:56:24 +0100

    updated syncfusion license

--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import { registerLicense } from '@syncfusion/ej2-base';
 import { ContextProvider } from './contexts/ContextProvider';
 import './index.css';
 import App from './App';
 
+registerLicense(import.meta.env.VITE_SYNCFUSION_LICENSE);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -14,5 +16,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <App />
       </Router>
     </ContextProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
